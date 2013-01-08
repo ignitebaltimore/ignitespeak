bf3cf399942d6b5e336a73c4bc8c7a8268b642c6
Mike Subelsky <github@mikeshop.net>
2013-01-08 16:48:50 -0500

    disable xml

---
+++ config/initializers/disable_xml.rb
@@ -0,0 +1,3 @@
+# https://groups.google.com/forum/#!topic/rubyonrails-security/61bkgvnSGTQ/discussion
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::YAML)
