be1272d374fe7f10ae47c68f18c2d8e42c575ff5
Mike Subelsky <github@mikeshop.net>
2013-09-12 09:59:58 -0400

    obscure proposals with robots and dont allow retrieval of archived proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -65,7 +65,7 @@ class ProposalsController < ApplicationController
   attr_reader :proposal, :proposals
 
   def find_proposal
-    @proposal = Proposal.find_by_hash_code(params[:id])
+    @proposal = Proposal.where(archived: false).find_by_hash_code(params[:id])
 
     if @proposal.nil?
       flash[:error] = "We could not find that proposal"
