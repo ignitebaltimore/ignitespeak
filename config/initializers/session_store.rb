98eee47c001639cbfa83e5887fb697ce37ccf994
Mike Subelsky <github@mikeshop.net>
2015-03-31 20:25:47 -0400

    make session cookies httponly

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ignitespeak::Application.config.session_store :cookie_store, key: '_ignitespeak_session'
+Ignitespeak::Application.config.session_store :cookie_store, {
+  key: '_ignitespeak_session',
+  httponly: true
+}
