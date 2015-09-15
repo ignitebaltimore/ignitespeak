976ad87b2621a0569c7d7d1f1c6fbe3e123b23bd
Mike Subelsky <github@mikeshop.net>
2015-09-15 16:32:22 -0400

    avoid code climate warning msg

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+if ENV["CODECLIMATE_REPO_TOKEN"]
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 require "rubygems"
 
 ENV["RAILS_ENV"] ||= 'test'
