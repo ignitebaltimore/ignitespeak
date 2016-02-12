5e71cc46abbdd63305ee1d4c223439b7b90542c3
Nono <test@email.com>
2016-02-12 15:34:31 -0500

    Added new code to have a database for selected and not archived events

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Event, type: :controller do 
+#	it { should validate_presence_of :title}
+
+#       it is_expected.to respond_with :ok  do
+       it "OKKKKK" do
+          get :show
+       end
+
+end
+
+
