58b2a1467b419a8bca591b83246b991d6a593f66
Nono <test@email.com>
2016-03-20 05:23:34 -0400

    Style change use if in return

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,42 +5,40 @@ class Admin < ActiveRecord::Base
 
  def self.do_twitter_oauth(auth, signed_in_resource = nil)
   admin = Admin.where(:provider => auth.provider, :uid => auth.uid).first
-  if admin
-    return admin
-  else
-    registered_admin = Admin.where(:email => auth.uid + "@twitter.com").first
-    if registered_admin
-      return registered_admin
-    else
-      # create user
-      admin = Admin.create(name:auth.info.name,
-        			provider:auth.provider,
-        			uid:auth.uid,
-        			email:auth.uid+"@twitter.com",
-        			password:Devise.friendly_token[0,20]
-      )
-    end
-  end
+
+  return admin if admin
+
+  registered_admin = Admin.where(:email => auth.uid + "@twitter.com").first
+
+  return registered_admin if registered_admin
+ 
+  # create user
+  admin = Admin.create(name:auth.info.name,
+    			provider:auth.provider,
+    			uid:auth.uid,
+    			email:auth.uid+"@twitter.com",
+    			password:Devise.friendly_token[0,20]
+  )
  end
 
  def self.do_google_oauth2(access_token, signed_in_resource = nil)
     data = access_token.info
     admin = Admin.where(:provider => access_token.provider, :uid => access_token.uid ).first
-    if admin
-      return admin
-    else
-      registered_admin = Admin.where(:email => access_token.info.email).first
-      if registered_admin
-        return registered_admin
-      else
-        # create user
-        admin = Admin.create(name: data["name"],
-          			provider:access_token.provider,
-          			email: data["email"],
-          			uid: access_token.uid ,
-          			password: Devise.friendly_token[0,20]
-        )
-      end
-   end
+
+    return admin if admin
+
+    registered_admin = Admin.where(:email => access_token.info.email).first
+
+    return registered_admin if registered_admin
+
+    # create user
+    admin = Admin.create(name: data["name"],
+      			provider:access_token.provider,
+      			email: data["email"],
+      			uid: access_token.uid ,
+      			password: Devise.friendly_token[0,20]
+    )
  end
+
 end
+
