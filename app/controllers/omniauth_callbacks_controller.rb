e4ad82df262134f0e9c082c8acdca675ec04b12a
Nono <test@email.com>
2016-03-21 07:49:01 -0400

    DRY' omniauth callbacks duplicate code goes into one

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,25 +1,23 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    auth = env["omniauth.auth"]
-    @admin = Admin.do_twitter_oauth(request.env["omniauth.auth"],current_admin)
+    social_callback("twitter")
+    return
+  end
+
+  def google
+    social_callback("google")
+    return
+  end
+
+  def social_callback(provider)
+    @admin = Admin.do_generic_oauth(request.env["omniauth.auth"], current_admin, provider)
     if @admin.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
       sign_in_and_redirect @admin, :event => :authentication
     else
       session["devise.twitter_uid"] = request.env["omniauth.auth"]
-      redirect_to new_admin_registration_url
+      redirect_to new_admin_session_path
     end
   end
 
-  def google
-    @admin = Admin.do_google_oauth2(request.env["omniauth.auth"], current_admin)
-    if @admin.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @admin, :event => :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_admin_registration_url
-    end
-  end
 end
-
