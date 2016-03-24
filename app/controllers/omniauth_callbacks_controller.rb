2a2328d27032c7a8ee3e58ad9deacd60632f5ab1
Nono <test@email.com>
2016-03-24 09:34:08 -0400

    added rspec for testing authorization of users using twitter and google

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,43 +11,31 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def social_callback(provider)
     auth_det = request.env["omniauth.auth"]
-    puts "111111111111111111111111111111111111111111"
-    puts auth_det.info.nickname
-    puts auth_det.info.email
   
     # if both nickname and email are absent
     if auth_det.info.nickname.blank? and auth_det.info.email.blank?
       redirect_to root_path
       return
     end
-    puts "HERE1:"
-#    puts  Admin.where(:nickname => "Alas")
- #   puts Admin.where("admins.nickname IS NOT NULL")
-#    Admin.includes(:bar).where.not('bars.id' => nil)
-  #  puts k[:nickname]
-    puts ActiveRecord::Base.connection.execute("Select * from admins")
-#    puts "over"
+    
     # Twitter does not return email so would use nickname instead
     if auth_det.info.nickname.present? and !Admin.exists?(:nickname => auth_det.info.nickname) 
       puts "Nickname not found"
       redirect_to root_path
       return
     end
-    puts "2222222222222222222222222222222222"
+
     if auth_det.info.email.present? and !Admin.exists?(:email => auth_det.info.email) 
       puts "User not allowed"
       redirect_to root_path
       return
     end
 
-
     if auth_det.info.nickname.present? and !Admin.exists?(:nickname => auth_det.info.nickname) and uth_det.info.email.present? and !Admin.exists?(:email => auth_det.info.email)
       puts "Email and nickname not allowed"
       redirect_to root_path
       return
     end
-
-    puts "333333333333333333333333333333333333333333"
 
     @admin = Admin.do_generic_oauth(auth_det, current_admin, provider)
     if @admin.persisted?
--- spec/features/omniauth.rb
+++ spec/features/omniauth.rb
@@ -1,31 +1,82 @@
 require "spec_helper"
-require "rails_helper"
 require 'support/basic_authentication'
+
+require 'database_cleaner'
+
 
 describe "omniauth authentication", :type => :feature do
  describe "with authentication on", :js => true, :type => :controller do
-   it "twitter sign in button should get to the admin page" do
+   it "twitter sign in button should get to the admin page", :strategy => :truncation do
+     # get the omniauth hash
+     mock_twitter_authorized
+  
+     # create an admin user
+     ad = Admin.create!(:nickname => "Alas", :email =>"alas@twitter.com", :password => "1234567890")
+     ad.save!
+
      visit rails_admin.dashboard_path
      click_link "Sign in with Twitter"
-#     puts page.body
      expect(page).to have_content 'Site Administration'
+   end
+
+   it "twitter sign in button should redirect to proposal form", :strategy => :truncation do
+     # get the omniauth hash   
+     mock_twitter_unauthorized  
+ 
+     # create an admin user
+     ad = Admin.create!(:nickname => "Alas", :email =>"alas@twitter.com", :password => "1234567890")
+     ad.save!
+
+     visit rails_admin.dashboard_path
+     click_link "Sign in with Twitter"
+     expect(page).to have_content 'Your email address'
+   end
+
+   it "twitter sign in button should fail if database is empty" do
+     # get the omniauth hash
+     mock_google_unauthorized  
+
+     visit rails_admin.dashboard_path
+     click_link "Sign in with Twitter"
+     expect(page).to have_content 'Your email address'
    end
 
    it "google sign in button should get to the admin page" do
+     # get the omniauth hash
+     mock_google_authorized  
+
+     ad = Admin.create!(:nickname => "", :email =>"alas@gmail.com", :password => "1234567890")
+     ad.save!
+
      visit rails_admin.dashboard_path
      click_link "Sign in with Google"
-#     puts page.body
      expect(page).to have_content 'Site Administration'
    end
 
-#  include_context 'basic authentication'
-#  it "access the admin page" do
-#   # go to the admin page 
-#   visit rails_admin.dashboard_path
-#   # login to the admin page
-#   http_basic_authentication
-#   # verify if the admin page has been reached
-#   expect(current_path).to eq rails_admin.dashboard_path 
-#  end
+
+   it "google sign in button should redirect to proposal form i.e. fail if email is wrong" do
+     # get the omniauth hash
+     mock_google_unauthorized  
+
+     ad = Admin.create!(:nickname => "Alas", :email =>"alas@gmail.com", :password => "1234567890")
+     ad.save!
+
+     visit rails_admin.dashboard_path
+     click_link "Sign in with Google"
+     expect(page).to have_content 'Your email address'
+   end
+
+   it "google sign in button should fail if database is empty" do
+     # get the omniauth hash
+     mock_google_unauthorized  
+
+     visit rails_admin.dashboard_path
+     click_link "Sign in with Google"
+     expect(page).to have_content 'Your email address'
+   end
+
+
+
  end
+
 end
