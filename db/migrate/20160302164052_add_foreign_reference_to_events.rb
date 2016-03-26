12a140deb4804a8bc97923b2114a922256e0c74a
Nono <test@email.com>
2016-03-26 06:32:23 -0400

    Added on delete cascade to prevent orphaned proposals

--- db/migrate/20160302164052_add_foreign_reference_to_events.rb
+++ db/migrate/20160302164052_add_foreign_reference_to_events.rb
@@ -1,6 +1,6 @@
 class AddForeignReferenceToEvents < ActiveRecord::Migration
   def change
    add_reference :proposals, :event, index: true
-   add_foreign_key :proposals, :event 
+   add_foreign_key :proposals, :event, on_delete: :cascade
   end
 end
