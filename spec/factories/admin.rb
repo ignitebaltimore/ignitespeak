f4cb1afe0d8fe275457cbac6a462b0083162502b
Nono <test@email.com>
2016-03-26 02:52:49 -0400

    Added tests for authorization

---
+++ spec/factories/admin.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :admin do
+    nickname "A nick"
+    email "nick@email.com"
+    password "123456789"
+  end
+
+  factory :admin2 do
+    nickname "A"
+    email "a@email.com"
+    password "qwrt63876932179"
+  end
+
+end
+
